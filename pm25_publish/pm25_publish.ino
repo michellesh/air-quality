8249f7710fef1560f10b3833a51c5f6f7661f40b
Michelle Sharer <michellesharer@gmail.com>
2021-11-15 19:32:16 -0700

    Display AQI on screen

--- pm25_publish/pm25_publish.ino
+++ pm25_publish/pm25_publish.ino
@@ -1,12 +1,34 @@
 #include "config.h"
 #include "Adafruit_PM25AQI.h"
 #include <SoftwareSerial.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// ESP8266 pin number -> GPIO mapping
+#define D0  16
+#define D1  5
+#define D2  4
+#define D3  0
+#define D4  2
+#define D5  14
+#define D6  12
+#define D7  13
+#define D8  15
+
+#define SCREEN_WIDTH    128   // OLED display width, in pixels
+#define SCREEN_HEIGHT   64    // OLED display height, in pixels
+#define OLED_RESET      -1    // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS  0x3C  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
 SoftwareSerial pmSerial(2, 3);
 
 Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
 
 AdafruitIO_Feed *aqiFeed = io.feed("Base Camp AQI");
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
   // Wait for serial monitor to open
@@ -15,12 +37,25 @@ void setup() {
 
   pmSerial.begin(9600);
 
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.setTextColor(SSD1306_WHITE);
+  display.display();
+  delay(2000);
+
   connectSensor();
   connectAdafruitIO();
 }
 
 void loop() {
   io.run();
+  display.clearDisplay();
 
   PM25_AQI_Data data;
 
@@ -33,9 +68,20 @@ void loop() {
   Serial.print("PM 2.5: ");
   Serial.println(data.pm25_standard);
 
+  displayAQI(display, data.pm25_standard);
+
   aqiFeed->save(data.pm25_standard);
 
+  display.display();
   delay(5000);
+}
+
+void displayAQI(Adafruit_SSD1306 &d, uint16_t aqi) {
+  char buffer[16];
+  d.setTextSize(3);
+  d.setCursor(0, 20);
+  sprintf(buffer, "%d", aqi);
+  d.println(buffer);
 }
 
 void connectSensor() {
