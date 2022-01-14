a7ab9b05234eac0f2aa4619a407b4edded8ba13b
Michelle Sharer <michellesharer@gmail.com>
2022-01-13 21:22:19 -0700

    Display time and draw a box around the PM2.5 value

--- pm25_publish/pm25_publish.ino
+++ pm25_publish/pm25_publish.ino
@@ -5,6 +5,8 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 // ESP8266 pin number -> GPIO mapping
 #define D0  16
@@ -30,32 +32,26 @@ AdafruitIO_Feed *aqiFeed = io.feed("Base Camp AQI");
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-void setup() {
-  // Wait for serial monitor to open
-  Serial.begin(115200);
-  while (!Serial) delay(10);
+const long utcOffsetInSeconds = 6 * 3600; // Your time zone
+WiFiUDP ntpUDP; // Define NTP Client to get time
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+char buffer[30];
 
+void setup() {
+  Serial.begin(115200);
   pmSerial.begin(9600);
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.setTextColor(SSD1306_WHITE);
-  display.display();
-  delay(2000);
-
-  connectSensor();
+  initDisplay();
+  initSensor();
+  connectWiFi();
   connectAdafruitIO();
+  timeClient.begin();
 }
 
 void loop() {
   io.run();
   display.clearDisplay();
+  timeClient.update();
 
   PM25_AQI_Data data;
 
@@ -68,30 +64,51 @@ void loop() {
   Serial.print("PM 2.5: ");
   Serial.println(data.pm25_standard);
 
-  displayAQI(display, data.pm25_standard);
-
   aqiFeed->save(data.pm25_standard);
 
+  displayRect(display, data.pm25_standard);
+  displayAQI(display, data.pm25_standard);
+  displayTime(display);
+
   display.display();
-  delay(5000);
+  delay(2000);
+}
+
+void displayRect(Adafruit_SSD1306 &d, uint16_t aqi) {
+  display.drawRect(0, 15, 21, 32, SSD1306_WHITE);
+}
+
+void displayTime(Adafruit_SSD1306 &d) {
+  int hours = timeClient.getHours();
+  int minutes = timeClient.getMinutes();
+  int seconds = timeClient.getSeconds();
+  sprintf(buffer, "%d:%02d:%02d", hours, minutes, seconds);
+  Serial.print("Last Updated: ");
+  Serial.println(buffer);
+
+  d.setTextSize(1);
+  d.setCursor(0, 50);
+  d.println(buffer);
 }
 
 void displayAQI(Adafruit_SSD1306 &d, uint16_t aqi) {
   char buffer[16];
   d.setTextSize(3);
-  d.setCursor(0, 20);
+  d.setCursor(3, 20);
   sprintf(buffer, "%d", aqi);
   d.println(buffer);
 }
 
-void connectSensor() {
+void initSensor() {
   // Wait one second for sensor to boot up!
   delay(1000);
 
   // connect to the sensor over software serial
-  if (! aqi.begin_UART(&pmSerial)) {
+  if (!aqi.begin_UART(&pmSerial)) {
     Serial.println("Could not find PM 2.5 sensor!");
-    while (1) delay(10);
+    while (1) {
+      delay(10);
+    }
   }
 
   Serial.println("PM25 found!");
@@ -112,4 +129,32 @@ void connectAdafruitIO() {
   // we are connected
   Serial.println();
   Serial.println(io.statusText());
+}
+
+void connectWiFi() {
+  WiFi.begin(SECRET_SSID, SECRET_PASS);
+  Serial.print("Connecting to ");
+  Serial.println(SECRET_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected!");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
+
+void initDisplay() {
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.setTextColor(SSD1306_WHITE);
+  display.display();
+  delay(2000);
 }
