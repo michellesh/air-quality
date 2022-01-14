363bb34b81df714b73823396ce304de9f38c15b4
Michelle Sharer <michellesharer@gmail.com>
2022-01-13 22:39:21 -0700

    move init functions to separate file

---
+++ pm25_publish/init.ino
@@ -0,0 +1,59 @@
+void initDisplay() {
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.setTextColor(SSD1306_WHITE);
+  display.display();
+  delay(2000);
+}
+
+void initSensor() {
+  // Wait one second for sensor to boot up!
+  delay(1000);
+
+  // connect to the sensor over software serial
+  if (!aqi.begin_UART(&pmSerial)) {
+    Serial.println("Could not find PM 2.5 sensor!");
+    while (1) {
+      delay(10);
+    }
+  }
+
+  Serial.println("PM25 found!");
+}
+
+void connectWiFi() {
+  WiFi.begin(SECRET_SSID, SECRET_PASS);
+  Serial.print("Connecting to ");
+  Serial.println(SECRET_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected!");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
+
+void connectAdafruitIO() {
+  Serial.print("Connecting to Adafruit IO");
+
+  // connect to io.adafruit.com
+  io.connect();
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+}
--- pm25_publish/pm25_publish.ino
+++ pm25_publish/pm25_publish.ino
@@ -93,7 +93,6 @@ void displayAQI() {
   displayNowText(display);
   displayNowAQI(display, data.pm25_standard);
   displayTime(display);
-
   display.display();
 }
 
@@ -135,64 +134,4 @@ void displayNowAQI(Adafruit_SSD1306 &d, uint16_t aqi) {
   char buffer[16];
   sprintf(buffer, "%d", aqi);
   d.println(buffer);
-}
-
-void initSensor() {
-  // Wait one second for sensor to boot up!
-  delay(1000);
-
-  // connect to the sensor over software serial
-  if (!aqi.begin_UART(&pmSerial)) {
-    Serial.println("Could not find PM 2.5 sensor!");
-    while (1) {
-      delay(10);
-    }
-  }
-
-  Serial.println("PM25 found!");
-}
-
-void connectAdafruitIO() {
-  Serial.print("Connecting to Adafruit IO");
-
-  // connect to io.adafruit.com
-  io.connect();
-
-  // wait for a connection
-  while(io.status() < AIO_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-
-  // we are connected
-  Serial.println();
-  Serial.println(io.statusText());
-}
-
-void connectWiFi() {
-  WiFi.begin(SECRET_SSID, SECRET_PASS);
-  Serial.print("Connecting to ");
-  Serial.println(SECRET_SSID);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected!");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-}
-
-void initDisplay() {
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.setTextColor(SSD1306_WHITE);
-  display.display();
-  delay(2000);
 }
